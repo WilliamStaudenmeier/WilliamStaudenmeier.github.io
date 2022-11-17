4533c7e0538474124ae10a2601b9974d6a4179d0
William Staudenmeier <40339355+WilliamStaudenmeier@users.noreply.github.com>
2022-11-16 20:52:13 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 let hrElement;
-let counter = 200;
+let counter = 50;
 for (let i = 0; i < counter; i++) {
   hrElement = document.createElement("HR");
   if (i == counter - 1) {
     hrElement.className = "thunder";
   } else {
     hrElement.style.left = Math.floor(Math.random() * window.innerWidth) + "px";
-    hrElement.style.animationDuration = 0.2 + Math.random() * 0.3 + "s";
+    hrElement.style.animationDuration = 0.4 + Math.random() * 0.3 + "s";
     hrElement.style.animationDelay = Math.random() * 5 + "s";
   }
   document.body.appendChild(hrElement);
