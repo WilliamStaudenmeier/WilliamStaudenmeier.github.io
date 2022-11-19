98c70c2783be9bbf64866cf5405336eca93fba69
William Staudenmeier <40339355+WilliamStaudenmeier@users.noreply.github.com>
2022-11-19 12:43:53 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,24 @@
-let hrElement;
-let counter = 500;
-for (let i = 0; i < counter; i++) {
-  hrElement = document.createElement("HR");
-  if (i == counter - 1) {
-    hrElement.className = "thunder";
-  } else {
-    hrElement.style.left = Math.floor(Math.random() * window.innerWidth) + "px";
-    hrElement.style.animationDuration = 0.4 + Math.random() * 0.1 + "s";
-    hrElement.style.animationDelay = Math.random() * 6 + "s";
+function toggleText(element, texts, delay) {
+  let i = 0;
+  
+  const work = () => {
+    element.innerHTML = texts[i].text;
+
+    setTimeout(work, (texts[i].delay * 1000))
+
+    i = (i < texts.length - 1) ? i + 1 : 0;
   }
-  document.body.appendChild(hrElement);
+
+  setTimeout(work, (delay * 1000))
 }
+
+toggleText(document.querySelector(".js-dialog"), [
+  {
+    text: '— Git...',
+    delay: 1.6
+  },
+  {
+    text: '...Hub',
+    delay: 2.4
+  }
+], 2);
